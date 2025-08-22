99b7eefd48311f859fa22775e076b541b52d84bf
Romano Foti <romanofoti@github.com>
2025-08-22 19:45:49 +0100

    fix" logger

--- doppiozero/agent/utils.py
+++ doppiozero/agent/utils.py
@@ -7,6 +7,54 @@ import os
 import json
 import logging
 
+# **********************************
+# Constants and parameters
+# **********************************
+
+FORMAT = "%(asctime)s - %(name)-10s - %(filename)-18s - %(funcName)-12s - %(levelname)-8s - %(message)s"  # noqa
+
+logging.basicConfig(format=FORMAT, level=logging.INFO)
+
+
+# **********************************
+# Functions definition
+# **********************************
+
+
+def get_logger(name=None, level=logging.INFO, format=FORMAT):
+    """Return a logger of the specified name and level.
+
+    Args:
+        name   : the name of the logger. If no name is specified, root logger is returned.
+        level  : logging levels (e.g. logging.INFO, logging.WARNING, etc.)
+        format : format for the logger.
+
+    Multiple calls to getLogger() with the same name return a reference to the same logger object.
+
+    """
+
+    # If no name is specified, return the root logger
+    if not name:
+        return logging.getLogger()
+
+    # If a logger with the specified name already exists, return it
+    logger = logging.getLogger(name)
+    if len(logger.handlers) != 0:
+        return logger
+
+    # If a logger with the specified name does not exist, create it
+    logger.setLevel(level)
+    formatter = logging.Formatter(format)
+
+    # Add a StreamHandler to the logger to enable logging to stdout
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    # set propagate to False to prevent the logger from propagating to the root logger
+    logger.propagate = False
+    return logger
+
 
 def read_json(path):
     with open(path, "r", encoding="utf-8") as f:
@@ -20,14 +68,3 @@ def write_json(path, data):
 
 def ensure_dir(path):
     os.makedirs(path, exist_ok=True)
-
-
-def setup_logger(name: str = "agent", level: int = logging.INFO):
-    logger = logging.getLogger(name)
-    if not logger.hasHandlers():
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s: %(message)s")
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-    logger.setLevel(level)
-    return logger
