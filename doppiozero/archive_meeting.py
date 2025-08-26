13143d0359dbdd0ecc67b48ba1066ef7e904f4ad
Romano Foti <romanofoti@github.com>
2025-08-26 21:48:16 +0100

    cleanup: remove archice meetings mdplide

--- doppiozero/archive_meeting.py
+++
@@ -1,9 +0,0 @@
-"""Compatibility wrapper: re-export meeting archival helpers from `meetings.py`."""
-
-from .meetings import (
-    find_transcript_files,
-    summarize_transcript,
-    archive_meeting,
-)
-
-__all__ = ["find_transcript_files", "summarize_transcript", "archive_meeting"]
