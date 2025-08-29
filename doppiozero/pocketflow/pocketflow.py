7c6416fb8a56cc9673901aef113edf9900cb851d
Romano Foti <romanofoti@github.com>
2025-08-29 22:24:35 +0200

    tmp: pkcgi

--- doppiozero/pocketflow/pocketflow.py
+++ doppiozero/pocketflow/pocketflow.py
@@ -6,15 +6,15 @@ import time
 
 class BaseNode:
     def __init__(self):
-        self.params, self.successors = {}, {}
+        self.param_dc, self.successor_dc = {}, {}
 
-    def set_params(self, params):
-        self.params = params
+    def set_param_dc(self, param_dc):
+        self.param_dc = param_dc
 
     def next(self, node, action="default"):
-        if action in self.successors:
+        if action in self.successor_dc:
             warnings.warn(f"Overwriting successor for action '{action}'")
-        self.successors[action] = node
+        self.successor_dc[action] = node
         return node
 
     def prep(self, shared):
@@ -35,7 +35,7 @@ class BaseNode:
         return self.post(shared, p, e)
 
     def run(self, shared):
-        if self.successors:
+        if self.successor_dc:
             warnings.warn("Node won't run successors. Use Flow.")
         return self._run(shared)
 
@@ -90,15 +90,15 @@ class Flow(BaseNode):
         return start
 
     def get_next_node(self, curr, action):
-        nxt = curr.successors.get(action or "default")
-        if not nxt and curr.successors:
-            warnings.warn(f"Flow ends: '{action}' not found in {list(curr.successors)}")
+        nxt = curr.successor_dc.get(action or "default")
+        if not nxt and curr.successor_dc:
+            warnings.warn(f"Flow ends: '{action}' not found in {list(curr.successor_dc)}")
         return nxt
 
     def _orch(self, shared, params=None):
-        curr, p, last_action = copy.copy(self.start_node), (params or {**self.params}), None
+        curr, p, last_action = copy.copy(self.start_node), (params or {**self.param_dc}), None
         while curr:
-            curr.set_params(p)
+            curr.set_param_dc(p)
             last_action = curr._run(shared)
             curr = copy.copy(self.get_next_node(curr, last_action))
         return last_action
@@ -116,7 +116,7 @@ class BatchFlow(Flow):
     def _run(self, shared):
         pr = self.prep(shared) or []
         for bp in pr:
-            self._orch(shared, {**self.params, **bp})
+            self._orch(shared, {**self.param_dc, **bp})
         return self.post(shared, pr, None)
 
 
@@ -144,7 +144,7 @@ class AsyncNode(Node):
                     await asyncio.sleep(self.wait)
 
     async def run_async(self, shared):
-        if self.successors:
+        if self.successor_dc:
             warnings.warn("Node won't run successors. Use AsyncFlow.")
         return await self._run_async(shared)
 
@@ -169,9 +169,9 @@ class AsyncParallelBatchNode(AsyncNode, BatchNode):
 
 class AsyncFlow(Flow, AsyncNode):
     async def _orch_async(self, shared, params=None):
-        curr, p, last_action = copy.copy(self.start_node), (params or {**self.params}), None
+        curr, p, last_action = copy.copy(self.start_node), (params or {**self.param_dc}), None
         while curr:
-            curr.set_params(p)
+            curr.set_param_dc(p)
             last_action = (
                 await curr._run_async(shared) if isinstance(curr, AsyncNode) else curr._run(shared)
             )
@@ -191,12 +191,12 @@ class AsyncBatchFlow(AsyncFlow, BatchFlow):
     async def _run_async(self, shared):
         pr = await self.prep_async(shared) or []
         for bp in pr:
-            await self._orch_async(shared, {**self.params, **bp})
+            await self._orch_async(shared, {**self.param_dc, **bp})
         return await self.post_async(shared, pr, None)
 
 
 class AsyncParallelBatchFlow(AsyncFlow, BatchFlow):
     async def _run_async(self, shared):
         pr = await self.prep_async(shared) or []
-        await asyncio.gather(*(self._orch_async(shared, {**self.params, **bp}) for bp in pr))
+        await asyncio.gather(*(self._orch_async(shared, {**self.param_dc, **bp}) for bp in pr))
         return await self.post_async(shared, pr, None)
